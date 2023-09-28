45a95b3e87e7fce8b9e2dce2dd19fd42121a1b70
Elrak01 <61857848+Elrak01@users.noreply.github.com>
2023-09-28 13:29:33 -0700

    finished

--- src/main.c
+++ src/main.c
@@ -14,16 +14,12 @@
  * The letters on the key are treated as numbers by their ordinality in the alphabet
  * The plaintext is then shifted on the alphabet by that amount moving the plaintext and key one-by-one
  * 
- * This implementation will support keys of different lengths than the plaintext by wrapping around on either
+ * This implementation will support keys of shorter lengths than the plaintext
  * 
  */
 
 #include <stdio.h>
 #include <ctype.h>
-
-int alphabetic_ordinality(char ch) {
-    return (tolower(ch) - 'a');
-}
 
 char alphabetic_shift(char ch, int sh) {
     char unchshch = tolower(ch) + sh; //unchecked shifted character
@@ -33,19 +29,48 @@ char alphabetic_shift(char ch, int sh) {
     return unchshch;
 }
 
+int vignere(char *plaintext, char *key, char *buffer) {
+
+    int ptlen = strlen(plaintext);
+    int klen = strlen(key);
+
+    for (int i = 0, k = 0; i < ptlen; i++) {
+        //reset k on short keys
+        if (k >= klen) k = 0;
+        if (isalpha(plaintext[i])) {
+
+            buffer[i] = alphabetic_shift(
+                plaintext[i], 
+                tolower(key[k]) - 'a'
+            );
+            k++;
+
+        } else buffer[i] = plaintext[i];
+    }
+
+    return 0;
+}
+
 int main(int argc, char const *argv[]) {
 
     char plaintext[4096];
     char key[4096];
     char ciphertext[4096];
 
-    printf("Please, input your plaintext below: \n");
+    printf("VIGNERE CIPHER\n\n\nPlease, input your plaintext below: \n");
     fgets(plaintext, sizeof(plaintext), stdin);
 
-    printf("Thank you, now input a key: \n");
+    printf("\nThank you, now input a key: \n");
     scanf("%s", &key);
 
-    printf("Thank you, running vignere cipher with parameters\n %s \nand\n %s", plaintext, key);
+    printf("\nThank you, running vignere cipher with parameters\n %sand\n %s", plaintext, key);
+
+    vignere(plaintext, key, ciphertext);
+
+    printf("\n\nCiphertext:\n%s", ciphertext);
+
+    scanf("%s");
+
 
     return 0;
 }
